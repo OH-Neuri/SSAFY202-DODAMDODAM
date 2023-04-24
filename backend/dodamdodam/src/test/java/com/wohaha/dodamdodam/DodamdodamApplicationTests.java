3ebec256e4df318f4d26d7b95a5da432194356c1
yunsuhee <suheeyun97@gmail.com>
2023-04-24 10:20:41 +0900

    프로젝트 구조

---
+++ backend/dodamdodam/src/test/java/com/wohaha/dodamdodam/DodamdodamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wohaha.dodamdodam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodamdodamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
