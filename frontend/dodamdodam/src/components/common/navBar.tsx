52b034f7699b9fd177c98351170be582694555b3
김나현 <skgusdl7403@naver.com>
2023-04-24 16:46:34 +0900

    Feat: 로그인, 회원가입, 네브바 구현

---
+++ frontend/dodamdodam/src/components/common/navBar.tsx
@@ -0,0 +1,37 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+export default function NavBar(props: {target: string}) {
+    const {target} = props
+    const router = useRouter()
+    const logout = () => {
+        alert('로그아웃')
+    }
+
+  return (
+    <div className='flex flex-col w-full h-screen bg-b_yellow'>
+        <div className='flex justify-center w-full my-10'>
+            <img onClick={()=>{router.push('/')}} className='w-1/2 cursor-pointer' src="/images/logo.png" alt="" />
+        </div>
+        <div>
+            <div onClick={()=>{router.push('각자 url 입력')}} className={`flex justify-center items-center w-full h-[80px] cursor-pointer hover:bg-yellow-500/20 text-[22px] ${target == '일정' && 'font-preM bg-yellow-500/20'}`}>일정 관리</div>
+            <div onClick={()=>{router.push('각자 url 입력')}} className={`flex justify-center items-center w-full h-[80px] cursor-pointer hover:bg-yellow-500/20 text-[22px] ${target == '급식' && 'font-preM bg-yellow-500/20'}`}>급식 관리</div>
+            <div onClick={()=>{router.push('각자 url 입력')}} className={`flex justify-center items-center w-full h-[80px] cursor-pointer hover:bg-yellow-500/20 text-[22px] ${target == '교사' && 'font-preM bg-yellow-500/20'}`}>교사 관리</div>
+            <div onClick={()=>{router.push('각자 url 입력')}} className={`flex justify-center items-center w-full h-[80px] cursor-pointer hover:bg-yellow-500/20 text-[22px] ${target == '반' && 'font-preM bg-yellow-500/20'}`}>반 관리</div>
+            <div onClick={()=>{router.push('각자 url 입력')}} className={`flex justify-center items-center w-full h-[80px] cursor-pointer hover:bg-yellow-500/20 text-[22px] ${target == '원생' && 'font-preM bg-yellow-500/20'}`}>원생 관리</div>
+        </div>
+        <div className='relative bottom-[-25%] flex justify-center w-full'>
+            <div className='flex items-center px-3 w-[90%] bg-yellow-400/50 h-[90px] rounded-full'>
+                <div className='rounded-full overflow-hidden w-[60px] h-[60px] mr-4'>
+                    <img className='object-fit w-full h-full' src="/images/main/cloud.png" alt="" />
+                </div>
+                <div className='flex flex-col w-[120px]'>
+                    <div className='font-preM text-[18px]'>소정어린이집</div>
+                    <div>쿨냥이원장님</div>
+                </div>
+                <div onClick={()=>{logout()}} className='cursor-pointer'>💢</div>
+            </div>
+        </div>
+    </div>
+  )
+}
--- frontend/dodamdodam/tailwind.config.js
+++ frontend/dodamdodam/tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        b_yellow : '#FFF1A1',
+        m_yellow : '#F8CF1C',
+      },
       fontFamily: {
         preBl : ['preBl'],
         preB : ['preB'],
@@ -22,6 +26,7 @@ module.exports = {
         preR : ['preR'],
         preSB : ['preSB'],
         preT : ['preT'],
+        KCC : ['KCC'],
       }
     },
   },
