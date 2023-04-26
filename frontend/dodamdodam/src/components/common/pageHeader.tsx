d540148595fba2fe7a694bd3edba1781e0cf841b
김나현 <skgusdl7403@naver.com>
2023-04-26 13:43:02 +0900

    Feat: 반 페이지 교사 등록 모달 생성

--- frontend/dodamdodam/src/components/common/pageHeader.tsx
+++ frontend/dodamdodam/src/components/common/pageHeader.tsx
@@ -7,7 +7,7 @@ export default function PageHeader(props: { name: string }) {
     <div className="flex m-10">
       <div className="flex flex-col justify-center ml-3">
         <div className="text-[20px]">소정어린이집</div>
-        <div className="text-[36px] font-preB text-[#4F4F4F]">{name}</div>
+        <div className="text-[40px] font-preB text-[#4F4F4F]">{name}</div>
       </div>
     </div>
   );
--- frontend/dodamdodam/src/pages/class/index.tsx
+++ frontend/dodamdodam/src/pages/class/index.tsx
@@ -1,15 +1,18 @@
 import NavBar from '@/components/common/navBar'
 import { classType } from '@/types/classType'
-import { Chip } from '@mui/material'
+import { Chip, Modal } from '@mui/material'
 import EditNoteOutlinedIcon from '@mui/icons-material/EditNoteOutlined';
 import HighlightOffOutlinedIcon from '@mui/icons-material/HighlightOffOutlined';
 import CloseIcon from '@mui/icons-material/Close';
 import React, { useState } from 'react'
+import PageHeader from '@/components/common/pageHeader';
 
 export default function index() {
   const [name, setName] = useState<string>('')
   const [teacher, setTeacher] = useState<string|null>(null)
   const [idx, setIdx] = useState<number>(-1)
+
+  const [open, setOpen] = useState<boolean>(false)
 
   const classList : classType[] = [
     {class_seq: 0, name: '햇님반', teacher: '김교사', age: '5세, 6세'},
@@ -53,21 +56,18 @@ export default function index() {
   return (
     <>
       <div className='grid grid-cols-7'>
+
+        {/* 네브바 */}
         <div className='fixed'>
           <NavBar target='반' />
         </div>
         <div></div>
-        <div className='col-span-6 p-20'>
-          <div className='flex flex-col border-b mb-8 pl-8'>
-            <div className='text-[20px]'>소정어린이집</div>
-            <div className='text-[36px] font-preB'>반 목록</div>
-            <div className='flex h-[60px]'>
-              <span>
-                <span></span>
-              </span>
-            </div>
-          </div>
-          <div className='flex w-full'>
+
+        {/* 내용 */}
+        <div className='col-span-6 pt-4 px-20'>
+          <PageHeader name='반 목록' />
+          <hr />
+          <div className='flex w-full mt-20'>
             {/* 반 목록 */}
             <div className='w-[56%] px-8 h-[600px] overflow-y-scroll [&::-webkit-scrollbar]:w-[10px] [&::-webkit-scrollbar-thumb]:bg-[#D5D5D5] [&::-webkit-scrollbar-thumb]:rounded-[10px] [&::-webkit-scrollbar-track]:hidden'>
               {classList.map((c, i)=>{
@@ -79,7 +79,7 @@ export default function index() {
                     {c.teacher ?
                     <div className=''><span className='font-preM'>담당교사</span> : {c.teacher}</div>
                     :
-                    <span className='flex justify-center items-center w-[180px] font-preR px-4 py-2 rounded-full bg-red-300 cursor-pointer hover:bg-red-400'>담당 교사 연결하기</span>
+                    <span onClick={()=>setOpen(true)} className='flex justify-center items-center w-[180px] font-preR px-4 py-2 rounded-full bg-red-300 cursor-pointer hover:bg-red-400'>담당 교사 연결하기</span>
                     }
                     </div>
                     <div onClick={()=>{update(i)}}>
@@ -90,6 +90,7 @@ export default function index() {
                 )
               })}
             </div>
+
             {/* 반 추가하기 */}
             <div className='w-[40%] flex flex-col items-center rounded shadow-lg px-10 ml-10'>
               {idx == -1 ?
@@ -111,6 +112,8 @@ export default function index() {
               </>
               :
               <>
+
+              {/* 반 수정하기 */}
               <span onClick={()=>close()} className='cursor-pointer relative top-[10px] right-[-250px]'>
                 <CloseIcon className='' />
               </span>
@@ -127,19 +130,33 @@ export default function index() {
                     })}
                 </div>
                 <div className='font-preR text-grey-800 mt-5 font-preR'>담당 교사</div>
-                {}
+                {teacher ?
                 <div className='flex w-full mt-2 mb-8'>
-                  <input readOnly={true} className='outline-none bg-stone-200/50 rounded w-3/5 h-[50px] px-4 mr-4' value={teacher ? teacher : ''} type="text" />
+                  <input readOnly={true} className='outline-none bg-stone-200/50 rounded w-3/5 h-[50px] px-4 mr-4' value={teacher} type="text" />
                   <div className='flex justify-center items-center font-preR bg-stone-200 h-[50px] w-[38%] rounded cursor-pointer hover:bg-stone-300/70'>삭제하기</div>  
                 </div>
+                :
+                <input readOnly={true} className='outline-none bg-stone-200/50 rounded w-full h-[50px] mt-2 px-4 mr-4' value='미등록' type="text" />
+              }
                 <div className='flex justify-center items-center w-full h-[60px] bg-m_yellow rounded text-[18px] font-preM mt-[60px] cursor-pointer hover:bg-h_yellow'>수정하기</div>
               </div>
               </>
-              
             }
             </div>
           </div>
         </div>
+          <Modal className='flex justify-center items-center' open={open} onClose={()=>{setOpen(false)}}>
+            <div className='flex flex-col items-center p-10 bg-white rounded outline-none w-[500px] h-[400px]'>
+              <div className='font-preB text-[24px] mb-10'>담당 교사 등록하기</div>
+              <div className='flex flex-col w-full'>
+                <div>1. 담당 교사의 핸드폰 번호를 입력해주세요.</div>
+                <div>2. 문자로 인증코드가 전송됩니다.</div>
+                <div>3. 도담도담 앱으로 인증코드를 입력하면 자동으로 등록됩니다.</div>
+              </div>
+              <input className='outline-none bg-stone-200/50 rounded w-full h-[50px] mt-5 px-5' placeholder='ex) 01012341234' type="text" />
+              <div className='flex justify-center items-center w-full h-[60px] bg-m_yellow rounded text-[18px] font-preM mt-10 cursor-pointer hover:bg-h_yellow'>인증코드 전송하기</div>             
+            </div>
+          </Modal>
       </div>
     </>
   )
