e6dfd8df94e3b5f37f3b5dba37612464ad04dfc1
김지현 <rla0347@naver.com>
2023-04-27 14:34:20 +0900

    Feat : Domain 생성

---
+++ backend/dodamdodam/src/main/java/com/wohaha/dodamdodam/domain/Notice.java
@@ -0,0 +1,34 @@
+package com.wohaha.dodamdodam.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "tbl_notice")
+@Getter
+@NoArgsConstructor
+public class Notice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notice_seq", nullable = false)
+    private Long noticeSeq;
+
+    @Column(name = "class_seq", nullable = false)
+    private Long classSeq;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updatedAt;
+
+}
