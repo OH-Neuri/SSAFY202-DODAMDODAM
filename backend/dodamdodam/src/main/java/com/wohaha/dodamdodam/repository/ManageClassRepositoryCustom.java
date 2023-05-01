f87bf894f7849b1a3a58a48e0bd7b2eeda508690
yunsuhee <suheeyun97@gmail.com>
2023-05-01 15:44:29 +0900

    Feat:반 등록 수정, 삭제

--- backend/dodamdodam/src/main/java/com/wohaha/dodamdodam/repository/ManageClassRepositoryCustom.java
+++ backend/dodamdodam/src/main/java/com/wohaha/dodamdodam/repository/ManageClassRepositoryCustom.java
@@ -1,13 +1,20 @@
 package com.wohaha.dodamdodam.repository;
 
 
+import com.wohaha.dodamdodam.dto.response.request.UpdateClassRequestDto;
 import com.wohaha.dodamdodam.dto.response.response.ClassListResponseDto;
 import com.wohaha.dodamdodam.dto.response.response.TeacherInfoResponseDto;
 
 import java.util.List;
 
 public interface ManageClassRepositoryCustom {
+    //반정보 조회
     List<ClassListResponseDto> classInfoList(long kindergartenSeq);
-
+    //선생님 정보
     List<TeacherInfoResponseDto> teacherInfo(long kindergartenSeq);
+    //반정보 업데이트
+    void updateClass(UpdateClassRequestDto updateClassRequestDto);
+    //반정보 삭제
+    void deleteClass(long classSeq);
+
 }
--- backend/dodamdodam/src/main/java/com/wohaha/dodamdodam/service/ManageClassService.java
+++ backend/dodamdodam/src/main/java/com/wohaha/dodamdodam/service/ManageClassService.java
@@ -1,14 +1,20 @@
 package com.wohaha.dodamdodam.service;
 
 import com.wohaha.dodamdodam.dto.response.request.CreateClassRequestDto;
+import com.wohaha.dodamdodam.dto.response.request.UpdateClassRequestDto;
 import com.wohaha.dodamdodam.dto.response.response.ClassListResponseDto;
 
 import java.util.List;
 
 public interface ManageClassService {
-
+    //반 생성
     boolean createClass(CreateClassRequestDto createClassRequestDto);
-
+    //반 조회
     List<ClassListResponseDto> classList();
+    //반 수정
+    boolean updateClass(UpdateClassRequestDto updateClassRequestDto);
+    //반 삭제
+    boolean deleteClass(long classSeq);
+
 
 }
