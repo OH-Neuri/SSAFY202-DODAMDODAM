9c22ffe48c01e78d45c39b25b9efcad2aba32b62
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2023-05-06 22:40:06 +0900

    Feat: 투약 의뢰서 학부모 디테일 페이지 생성

---
+++ app/lib/screens/medicine/medicine_parent_detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MedicineParentDetailPage extends StatefulWidget {
+
+  const MedicineParentDetailPage({Key? key}) : super(key: key);
+  _MedicineParentDetailPageState createState() => _MedicineParentDetailPageState();
+}
+
+class _MedicineParentDetailPageState extends State<MedicineParentDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
