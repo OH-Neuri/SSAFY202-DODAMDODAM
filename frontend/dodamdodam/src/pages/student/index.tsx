3ea9a177e470ca371b47dcf58a0fd332ff59f8cd
오하늘 <moho009804@gmail.com>
2023-05-08 17:50:12 +0900

    Feat: 원생 리스트 데이터 불러오기

--- frontend/dodamdodam/src/pages/student/index.tsx
+++ frontend/dodamdodam/src/pages/student/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import StudentCard from "@/components/student/studentCard";
 import { student } from "@/types/DataTypes";
 import NavBar from "@/components/common/navBar";
@@ -6,7 +6,9 @@ import Image from "next/image";
 import StudentRegisterModal from "@/components/student/studentRegisterModal";
 import PageHeader from "@/components/common/pageHeader";
 import StudentModifyModal from "@/components/student/studentModifyModal";
-
+import axios from "axios";
+import { QueryOptions, useQuery } from "react-query";
+import { BACKEND_URL } from "@/config/config";
 export default function index() {
   const [openRe, setOpenRe] = useState<boolean>(false);
   const [openMo, setOpenMo] = useState<boolean>(false);
@@ -18,6 +20,49 @@ export default function index() {
     setOpenMo(true)
   };
   const handleCloseMo = () => setOpenMo(false);
+ 
+  const StudentList = () => {
+    const { isLoading, isError, error, refetch, data } = useQuery("studentList", async () => {
+      const queryKey = `https://dodamdodam.site/api/dodam/kindergarten/kid`;
+      const response = await axios.get(queryKey);
+      return response.data.result;
+    }, {
+      retry: 0,
+      onSuccess: data => {
+        console.log(data);
+      },
+      onError: error => {
+        console.log(error)
+      },
+      staleTime: 1000 * 60 * 5,
+      cacheTime: 1000 * 60 * 5
+    });
+    if (isLoading) {
+      return <h1>Loading.......</h1>
+    }
+    if (isError) {
+      return <h1>에러발생</h1>
+    }
+    return(
+      <div>
+        {data&&data.map((v:any,i:any) => { 
+          return (
+            <div key={i}>
+              {v.birth}
+            </div>
+          )
+        })}
+      </div>
+    );
+  };
+ useQuery
+// options : 
+//  staleTime cacheTime 
+//  onError onSettled onSuccess select
+//  retry refetchOnMount refetchOnReconnect refetchOnWindowFocus retry, staleTime, 
+  useEffect(() => { 
+    StudentList
+  },[])
 
   const studentList: student[] = [
     {
@@ -93,8 +138,8 @@ export default function index() {
       <div className="col-span-1"></div>
       <div className="col-span-6 pl-20 pt-4">
         <PageHeader name={"원생 목록"}></PageHeader>
-        
-        {/* 교사 카드 */}
+        <StudentList/>
+        {/* 원생 카드 */}
         <div className=" grid grid-cols-6 gap-1 w-[1500px] h-[100px] mt-[100px] ml-10">
           {studentList.map((v, i) => {
             return (
@@ -109,7 +154,8 @@ export default function index() {
               </div>
             );
           })}
-        {/* 교사 등록 카드 */}
+
+        {/* 원생 등록 카드 */}
           <div
             onClick={() => handleOpenRe()}
             className=" cursor-pointer flex justify-center items-center hover:bg-gray-200 w-[200px] h-[250px] rounded-3xl border-1 border-gray-200 bg-gray-100  "
