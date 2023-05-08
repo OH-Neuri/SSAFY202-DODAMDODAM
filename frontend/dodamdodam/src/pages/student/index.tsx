642ba9db1eb3c6b613335e22a0ac91a30b405683
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2023-05-08 22:48:21 +0900

    Feat: 원생 페이지 API 연결 작업 중

--- frontend/dodamdodam/src/pages/student/index.tsx
+++ frontend/dodamdodam/src/pages/student/index.tsx
@@ -7,141 +7,47 @@ import StudentRegisterModal from "@/components/student/studentRegisterModal";
 import PageHeader from "@/components/common/pageHeader";
 import StudentModifyModal from "@/components/student/studentModifyModal";
 import axios from "axios";
+
 import { QueryOptions, useQuery } from "react-query";
 import { BACKEND_URL } from "@/config/config";
 export default function index() {
   const [openRe, setOpenRe] = useState<boolean>(false);
   const [openMo, setOpenMo] = useState<boolean>(false);
-  const [student, setStudent] = useState<number>(1)
+  const [student, setStudent] = useState<number>(1);
+  const [studentList, setStudentList] = useState<student[number] | any>();
+
   const handleOpenRe = () => setOpenRe(true);
   const handleCloseRe = () => setOpenRe(false);
-  const handleOpenMo = (v:number)=> {
-    setStudent(v)
-    setOpenMo(true)
+  const handleOpenMo = (v: number) => {
+    setStudent(v);
+    setOpenMo(true);
   };
   const handleCloseMo = () => setOpenMo(false);
- 
+
   const StudentList = () => {
-    const { isLoading, isError, error, refetch, data } = useQuery("studentList", async () => {
-      const queryKey = `https://dodamdodam.site/api/dodam/kindergarten/kid`;
-      const response = await axios.get(queryKey);
-      return response.data.result;
-    }, {
-      retry: 0,
-      onSuccess: data => {
-        console.log(data);
+    const { isLoading, isError, error, refetch, data } = useQuery(
+      "studentList",
+      async () => {
+        const queryKey = `https://dodamdodam.site/api/dodam/kindergarten/kid`;
+        const response = await axios.get(queryKey);
+        return response.data.result;
       },
-      onError: error => {
-        console.log(error)
-      },
-      staleTime: 1000 * 60 * 5,
-      cacheTime: 1000 * 60 * 5
-    });
+      {
+        retry: 0,
+        staleTime: 1000 * 60 * 5,
+        cacheTime: 1000 * 60 * 5,
+      }
+    );
     if (isLoading) {
-      return <h1>Loading.......</h1>
+      return <h1>Loading.......</h1>;
     }
     if (isError) {
-      return <h1>에러발생</h1>
+      return <h1>에러발생</h1>;
     }
-    return(
-      <div>
-        {data&&data.map((v:any,i:any) => { 
-          return (
-            <div key={i}>
-              {v.birth}
-            </div>
-          )
-        })}
-      </div>
-    );
-  };
- useQuery
-// options : 
-//  staleTime cacheTime 
-//  onError onSettled onSuccess select
-//  retry refetchOnMount refetchOnReconnect refetchOnWindowFocus retry, staleTime, 
-  useEffect(() => { 
-    StudentList
-  },[])
-
-  const studentList: student[] = [
-    {
-      image: "/images/student/dog1.jpg",
-      name: "여니",
-      birth:"2018-04-06",
-      gender: "여자",
-      class: "꽃님반",
-    },
-    {
-      image: "/images/student/squirrel.jpg",
-      name: "느리스키",
-      birth:"2018-04-06",
-      gender: "남자",
-      class: "햇살반",
-    },
-    {
-      image: "/images/student/dog1.jpg",
-      name: "줼레나",
-      birth:"2018-04-06",
-      gender: "여자",
-      class: "나무반",
-    },
-    {
-      image: "/images/student/squirrel.jpg",
-      name: "줼레나",
-      birth:"2018-04-06",
-      gender: "여자",
-      class: "나무반",
-    },
-    {
-      image: "/images/student/European_otter.jpg",
-      name: "줼레나",
-      birth:"2018-04-06",
-      gender: "여자",
-      class: "나무반",
-    },
-    {
-      image: "/images/student/squirrel.jpg",
-      name: "줼레나",
-      birth:"2018-04-06",
-      gender: "여자",
-      class: "나무반",
-    },
-    {
-      image: "/images/student/squirrel.jpg",
-      name: "줼레나",
-      birth:"2018-04-06",
-      gender: "여자",
-      class: "나무반",
-    },
-    {
-      image: "/images/student/European_otter.jpg",
-      name: "줼레나",
-      birth:"2018-04-06",
-      gender: "여자",
-      class: "나무반",
-    },
-
-    {
-      image: "/images/student/squirrel.jpg",
-      name: "줼루나",
-      birth:"2018-04-06",
-      gender: "여자",
-      class: "나무반",
-    },
-  ];
-  return (
-    <div className=" grid grid-cols-7 h-[935px] ">
-      <div className="fixed">
-        <NavBar target="원생" />
-      </div>
-      <div className="col-span-1"></div>
-      <div className="col-span-6 pl-20 pt-4">
-        <PageHeader name={"원생 목록"}></PageHeader>
-        <StudentList/>
-        {/* 원생 카드 */}
-        <div className=" grid grid-cols-6 gap-1 w-[1500px] h-[100px] mt-[100px] ml-10">
-          {studentList.map((v, i) => {
+    return (
+      <>
+        {data &&
+          data.map((v: any, i: any) => {
             return (
               <div key={i}>
                 <div
@@ -149,13 +55,104 @@ export default function index() {
                     handleOpenMo(i);
                   }}
                 >
-                <StudentCard key={i} student={v}></StudentCard>
+                  <StudentCard key={i} student={v}></StudentCard>
                 </div>
               </div>
             );
           })}
+      </>
+    );
+  };
 
-        {/* 원생 등록 카드 */}
+  // useQuery;
+  // options :
+  //  staleTime cacheTime
+  //  onError onSettled onSuccess select
+  //  retry refetchOnMount refetchOnReconnect refetchOnWindowFocus retry, staleTime,
+
+  useEffect(() => {
+    StudentList;
+  }, []);
+
+  // const studentList: student[] = [
+  //   {
+  //     image: "/images/student/dog1.jpg",
+  //     name: "여니",
+  //     birth: "2018-04-06",
+  //     gender: "여자",
+  //     class: "꽃님반",
+  //   },
+  //   {
+  //     image: "/images/student/squirrel.jpg",
+  //     name: "느리스키",
+  //     birth: "2018-04-06",
+  //     gender: "남자",
+  //     class: "햇살반",
+  //   },
+  //   {
+  //     image: "/images/student/dog1.jpg",
+  //     name: "줼레나",
+  //     birth: "2018-04-06",
+  //     gender: "여자",
+  //     class: "나무반",
+  //   },
+  //   {
+  //     image: "/images/student/squirrel.jpg",
+  //     name: "줼레나",
+  //     birth: "2018-04-06",
+  //     gender: "여자",
+  //     class: "나무반",
+  //   },
+  //   {
+  //     image: "/images/student/European_otter.jpg",
+  //     name: "줼레나",
+  //     birth: "2018-04-06",
+  //     gender: "여자",
+  //     class: "나무반",
+  //   },
+  //   {
+  //     image: "/images/student/squirrel.jpg",
+  //     name: "줼레나",
+  //     birth: "2018-04-06",
+  //     gender: "여자",
+  //     class: "나무반",
+  //   },
+  //   {
+  //     image: "/images/student/squirrel.jpg",
+  //     name: "줼레나",
+  //     birth: "2018-04-06",
+  //     gender: "여자",
+  //     class: "나무반",
+  //   },
+  //   {
+  //     image: "/images/student/European_otter.jpg",
+  //     name: "줼레나",
+  //     birth: "2018-04-06",
+  //     gender: "여자",
+  //     class: "나무반",
+  //   },
+
+  //   {
+  //     image: "/images/student/squirrel.jpg",
+  //     name: "줼루나",
+  //     birth: "2018-04-06",
+  //     gender: "여자",
+  //     class: "나무반",
+  //   },
+  // ];
+  return (
+    <div className=" grid grid-cols-7 h-[935px] ">
+      <div className="fixed">
+        <NavBar tarStudentCardget="원생" />
+      </div>
+      <div className="col-span-1"></div>
+      <div className="col-span-6 pl-20 pt-4">
+        <PageHeader name={"원생 목록"}></PageHeader>
+        {/* 원생 카드 */}
+        <div className=" grid grid-cols-6 gap-1 w-[1500px] h-[100px] mt-[100px] ml-10">
+          <StudentList />
+
+          {/* 원생 등록 카드 */}
           <div
             onClick={() => handleOpenRe()}
             className=" cursor-pointer flex justify-center items-center hover:bg-gray-200 w-[200px] h-[250px] rounded-3xl border-1 border-gray-200 bg-gray-100  "
@@ -176,7 +173,8 @@ export default function index() {
           <div className="">
             <StudentModifyModal
               open={openMo}
-              student={studentList[student]}
+              idx={student}
+              // student={studentList[0]}
               handleOpen={handleOpenMo}
               handleClose={handleCloseMo}
             ></StudentModifyModal>
