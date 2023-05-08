ec5196ce86af4faf734133a97e9fe64f57c23302
이연희 <leeuh7262@naver.com>
2023-05-08 14:22:41 +0900

    Feat: device 정보 담는 컨트롤러 추가

--- app/lib/components/common/main_icon_button.dart
+++ app/lib/components/common/main_icon_button.dart
@@ -1,27 +1,59 @@
-import 'package:app/screens/notice/notice_list.dart';
+import 'package:app/controller/deviceInfo_controller.dart';
+import 'package:app/screens/attendance/attendance_detail_parent.dart';
+import 'package:app/screens/attendance/attendance_list.dart';
+import 'package:app/screens/medicine/medicine_parent_page.dart';
+import 'package:app/screens/medicine/medicine_teacher_page.dart';
 import 'package:flutter/material.dart';
+import 'package:app/screens/notice/notice_list.dart';
 import 'package:get/get.dart';
 
-class MainIconButton extends StatelessWidget {
+class MainIconButton extends StatefulWidget {
   final String title;
   const MainIconButton({super.key, required this.title});
 
   @override
+  State<MainIconButton> createState() => _MainIconButtonState();
+}
+
+class _MainIconButtonState extends State<MainIconButton> {
+  bool isTeacher = false;
+  DeviceInfoController c = Get.put(DeviceInfoController());
+
+  @override
+  void initState() {
+    super.initState();
+    isTeacher = c.isTeacher.value;
+  }
+  @override
   Widget build(BuildContext context) {
     Widget icon;
     dynamic link;
-    switch (title) {
+    switch (widget.title) {
       case '알림장':
         icon = Image.asset('/images/common/notebook_icon.png');
         link = NoticeList();
         break;
       case '등하원 확인서':
         icon = Image.asset('/images/common/goback_icon.png');
-        link = NoticeList();
+        // 선생님이면?
+        if (isTeacher) {
+          link = AttendanceList();
+        }
+        // 부모면?
+        else {
+          link = AttendanceDetailParent();
+        }
         break;
       case '투약 의뢰서':
         icon = Image.asset('/images/common/medicine_icon.png');
-        link = NoticeList();
+        // 선생님이면?
+        if (isTeacher) {
+          link = MedicineTeacherPage();
+        }
+        // 부모면?
+        else {
+          link = MedicineParentPage();
+        }
         break;
       default:
         icon = Container();
@@ -38,27 +70,27 @@ class MainIconButton extends StatelessWidget {
         height: 100, width: double.infinity,
         margin: EdgeInsets.symmetric(horizontal: 15),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 0,
-                blurRadius: 5.0,
-                offset: Offset(0, 10),
-              ),
-            ],
-          ),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 0,
+              blurRadius: 5.0,
+              offset: Offset(0, 10),
+            ),
+          ],
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
-              width: 50, height: 50,
-              child: icon
+                width: 50, height: 50,
+                child: icon
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
-              child: Text(title),
+              child: Text(widget.title),
             ),
           ],
         ),
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,15 +1,11 @@
-import 'package:app/firebase_options.dart';
+import 'package:app/controller/root_controller.dart';
+import 'package:app/root.dart';
 import 'package:app/screens/attendance/attendance_detail_teacher.dart';
-import 'package:app/screens/notice/notice_ai.dart';
-import 'package:app/screens/medicine/medicine_parent_regist_page.dart';
-import 'package:app/screens/medicine/medicine_parent_page.dart';
-import 'package:app/screens/medicine/medicine_detail_page.dart';
-import 'package:app/screens/medicine/medicine_teacher_page.dart';
+import 'package:app/screens/main/main_page.dart';
 import 'package:app/screens/notice/notice_detail.dart';
 import 'package:app/screens/notice/notice_image_detail.dart';
 import 'package:app/screens/notice/notice_list.dart';
 import 'package:app/screens/notice/notice_regist.dart';
-import 'package:app/screens/notice/select_kid.dart';
 import 'package:app/screens/user/login_select.dart';
 import 'package:app/screens/user/login_teacher.dart';
 import 'package:app/screens/attendance/attendance_list.dart';
@@ -18,7 +14,6 @@ import 'package:app/screens/attendance/attendance_detail_parent.dart';
 import 'package:app/screens/user/signup_id.dart';
 import 'package:app/screens/user/signup_number.dart';
 import 'package:app/screens/user/signup_select.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -32,80 +27,75 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: NoticeList(),
-      theme: ThemeData(fontFamily: 'Pretendard'),
-      // initialRoute: '/',
-      getPages: [
-        GetPage(name: '/', page: () => LoginSelect()),
-        GetPage(
-            name: '/login/teacher',
-            page: () => LoginTeacher(),
-            transition: Transition.rightToLeft),
-        GetPage(
-            name: '/signup/select',
-            page: () => SignupSelect(),
-            transition: Transition.rightToLeft),
-        GetPage(
-            name: '/signup/id',
-            page: () => SignupId(),
-            transition: Transition.rightToLeft),
-        GetPage(
-            name: '/signup/number',
-            page: () => SignupNumber(),
-            transition: Transition.rightToLeft),
-        GetPage(
-            name: '/notice',
-            page: () => NoticeList(),
-            transition: Transition.cupertino),
-        GetPage(
-            name: '/notice/detail',
-            page: () => NoticeDetail(),
-            transition: Transition.cupertino),
-        GetPage(
-            name: '/notice/regist',
-            page: () => NoticeRegist(),
-            transition: Transition.cupertino),
-        GetPage(
-            name: '/notice/regist/ai',
-            page: () => NoticeAI(),
-            transition: Transition.cupertino),
-        GetPage(
-            name: '/notice/regist/kid',
-            page: () => SelectKid(),
-            transition: Transition.cupertino),
-        GetPage(
-            name: '/notice/image/:idx',
-            page: () => NoticeImageDetail(),
-            transition: Transition.cupertino),
-        GetPage(
-            name: '/attendance',
-            page: () => AttendanceList(),
-            transition: Transition.cupertino),
-        GetPage(
-            name: '/attendance/teacher',
-            page: () => AttendanceDetailTeacher(),
-            transition: Transition.cupertino),
-        GetPage(
-            name: '/attendance/parent',
-            page: () => AttendanceDetailParent(),
-            transition: Transition.cupertino),
-        GetPage(
-            name: '/medicine/teacher',
-            page: () => MedicineTeacherPage(),
-            transition: Transition.cupertino),
-        GetPage(
-            name: '/medicine/detail',
-            page: () => MedicineDetailPage(),
-            transition: Transition.cupertino),
-        GetPage(
-            name: '/medicine/parent',
-            page: () => MedicineParentPage(),
-            transition: Transition.cupertino),
-        GetPage(
-            name: '/medicine/parent/regist',
-            page: () => MedicineParentRegistPage(),
-            transition: Transition.cupertino),
-      ],
+      title: 'Do-damDo-dam',
+      initialBinding: BindingsBuilder(() {
+        Get.put(RootController());
+      }),
+      home: Root(),
+
+      // bottom Navi를 유지하지 않을 때는 , GetPage를 사용해서 이동.
+      // 로그인 하기 전일 때 처리하면 될듯.
     );
   }
 }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       home: MainPage(),
+//       theme: ThemeData(fontFamily: 'Pretendard'),
+//       // initialRoute: '/',
+//       getPages: [
+//         GetPage(name: '/', page: () => LoginSelect()),
+//         GetPage(
+//           name: '/main',
+//           page: () => MainPage(),
+//           transition: Transition.rightToLeft),
+//         GetPage(
+//             name: '/login/teacher',
+//             page: () => LoginTeacher(),
+//             transition: Transition.rightToLeft),
+//         GetPage(
+//             name: '/signup/select',
+//             page: () => SignupSelect(),
+//             transition: Transition.rightToLeft),
+//         GetPage(
+//             name: '/signup/id',
+//             page: () => SignupId(),
+//             transition: Transition.rightToLeft),
+//         GetPage(
+//             name: '/signup/number',
+//             page: () => SignupNumber(),
+//             transition: Transition.rightToLeft),
+//         GetPage(
+//             name: '/notice',
+//             page: () => NoticeList(),
+//             transition: Transition.cupertino),
+//         GetPage(
+//             name: '/notice/detail',
+//             page: () => NoticeDetail(),
+//             transition: Transition.cupertino),
+//         GetPage(
+//             name: '/notice/regist',
+//             page: () => NoticeRegist(),
+//             transition: Transition.cupertino),
+//         GetPage(
+//             name: '/notice/image/:idx',
+//             page: () => NoticeImageDetail(),
+//             transition: Transition.cupertino),
+//         GetPage(
+//             name: '/attendance',
+//             page: () => AttendanceList(),
+//             transition: Transition.cupertino),
+//         GetPage(
+//             name: '/attendance/teacher',
+//             page: () => AttendanceDetailTeacher(),
+//             transition: Transition.cupertino),
+//         GetPage(
+//             name: '/attendance/parent',
+//             page: () => AttendanceDetailParent(),
+//             transition: Transition.cupertino),
+//       ],
+//     );
+//   }
+// }
