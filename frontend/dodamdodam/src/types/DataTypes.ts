eb1bc5993a2e05b1433ada5b0bb0bfa3e3d0be05
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2023-05-08 22:45:20 +0900

    Feat: student 키 변경

--- frontend/dodamdodam/src/types/DataTypes.ts
+++ frontend/dodamdodam/src/types/DataTypes.ts
@@ -1,21 +1,23 @@
 export type student = {
-    [index: string]: string | number;
-    image: string;
-    name: string;
-    birth: string;
-    gender: string;
-    class: string;
+  [index: string]: string | number;
+  kidSeq: number;
+  photo: string;
+  kidName: string;
+  birth: string;
+  gender: string;
+  classSeq: string;
+  className: string;
 };
 
 export type teacher = {
-    [index: string]: string | number;
-    name: string;
-    class: string;
-}
+  [index: string]: string | number;
+  name: string;
+  class: string;
+};
 
 export type kindergartenType = {
-    [index: string] : string
-    name: string
-    address: string
-    phone_number: string
-}
+  [index: string]: string;
+  name: string;
+  address: string;
+  phone_number: string;
+};
