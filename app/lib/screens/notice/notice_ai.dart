91d64f758133eed70071216660d9c2e43ae1a673
김나현 <skgusdl7403@naver.com>
2023-05-13 01:45:07 +0900

    Design: 알림장 자동 완성 페이지 크기 조절 및 컴포넌트화

--- app/lib/screens/notice/notice_ai.dart
+++ app/lib/screens/notice/notice_ai.dart
@@ -1,4 +1,5 @@
 import 'package:app/components/common/logout_app_bar.dart';
+import 'package:app/components/notice/ai_guide_modal.dart';
 import 'package:app/components/notice/keyword_chip.dart';
 import 'package:app/constants.dart';
 import 'package:flutter/material.dart';
@@ -27,12 +28,14 @@ class _NoticeAIState extends State<NoticeAI> {
           Expanded(child: SizedBox()),
           Flexible(
             flex: 12,
-            child: Column(
-              children: [
-                Expanded(
-                  child: Container(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    height: 460,
                     margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                    padding: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+                    padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(20),
                       color: Colors.white,
@@ -41,11 +44,14 @@ class _NoticeAIState extends State<NoticeAI> {
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            Text('알림장 자동완성', style: TextStyle(
-                              fontSize: subTitleTextSize,
-                              fontWeight: FontWeight.w600
+                            Padding(
+                              padding: const EdgeInsets.only(left: 74.0),
+                              child: Text('알림장 자동완성', style: TextStyle(
+                                fontSize: subTitleTextSize,
+                                fontWeight: FontWeight.w600
+                                ),
                               ),
                             ),
                             TextButton(
@@ -59,69 +65,7 @@ class _NoticeAIState extends State<NoticeAI> {
                                     ),
                                     context: context,
                                     builder: (BuildContext context) {
-                                      return Padding(
-                                        padding: const EdgeInsets.all(50.0),
-                                        child: Column(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            Text('작성하기 전', style: TextStyle(
-                                              fontSize: contentTextSize,
-                                              fontWeight: FontWeight.w800,
-                                              color: logoNavy
-                                            )),
-                                            Text('키워드 작성하기', style: TextStyle(
-                                              fontSize: titleTextSize,
-                                              fontWeight: FontWeight.w700
-                                            ),),
-                                            Padding(
-                                              padding: const EdgeInsets.fromLTRB(0, 30, 0, 20),
-                                              child: Text('- 언제, 어디서, 무엇을 했는지(할 예정인지) 적어주세요.',
-                                                style: TextStyle(
-                                                  fontSize: buttonTextSize,
-                                                  fontWeight: FontWeight.w500
-                                                ),
-                                              ),
-                                            ),
-                                            Padding(
-                                              padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-                                              child: Text('- 키워드가 자세할수록 정확한 내용이 나옵니다.',
-                                                style: TextStyle(
-                                                  fontSize: buttonTextSize,
-                                                  fontWeight: FontWeight.w500
-                                                ),
-                                              ),
-                                            ),
-                                            Padding(
-                                              padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-                                              child: Text('- 키워드의 순서도 영향을 미칠 수 있습니다.',
-                                                style: TextStyle(
-                                                  fontSize: buttonTextSize,
-                                                  fontWeight: FontWeight.w500
-                                                ),
-                                              ),
-                                            ),
-                                            Padding(
-                                              padding: const EdgeInsets.only(top: 12, bottom: 16),
-                                              child: Text('  (예시)', style: TextStyle(fontSize: contentTextSize),),
-                                            ),
-                                            SizedBox(
-                                              width: double.infinity,
-                                              child: Wrap(
-                                                alignment: WrapAlignment.start,
-                                                spacing: 6,
-                                                runSpacing: 10,
-                                                children: [
-                                                  KeywordChip(text: '봄 소풍', onPressed: (){}),
-                                                  KeywordChip(text: '준비물은 도시락', onPressed: (){}),
-                                                  KeywordChip(text: '9시에 출발하고 16시에 도착 예정', onPressed: (){}),
-                                                  KeywordChip(text: '소풍 장소는 서울숲', onPressed: (){}),
-                                                  KeywordChip(text: '즐겁게 잘 놀다 옴', onPressed: (){}),
-                                                ],
-                                              ),
-                                            )
-                                          ],
-                                        ),
-                                      );
+                                      return AIGuideModal();
                                     }
                                 );
                               },
@@ -168,7 +112,7 @@ class _NoticeAIState extends State<NoticeAI> {
                                 style: ElevatedButton.styleFrom(
                                   backgroundColor: darkNavy,
                                   foregroundColor: Colors.white,
-                                  padding: EdgeInsets.all(10),
+                                  padding: EdgeInsets.all(4),
                                   shape: CircleBorder(
                                     eccentricity: 0
                                   )
@@ -182,7 +126,7 @@ class _NoticeAIState extends State<NoticeAI> {
                                     inputFocus.requestFocus();
                                   });
                                 },
-                                child: Icon(Icons.add)
+                                child: Icon(Icons.add, size: 18,)
                               )
                             ],
                           ),
@@ -205,33 +149,33 @@ class _NoticeAIState extends State<NoticeAI> {
                         )
                       ],
                     ),
-                  )
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-                  child: ElevatedButton(
-                      onPressed: (){},
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: Size(double.infinity, 70),
-                        backgroundColor: darkNavy
-                      ),
-                      child: Text('작성하기',style: TextStyle(fontSize: buttonTextSize),)
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 14, 0, 30),
-                  child: ElevatedButton(
-                      onPressed: (){
-                        Get.back();
-                      },
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: Size(double.infinity, 70),
-                        backgroundColor: Color(0xffA2A2A2)
-                      ),
-                      child: Text('취소하기',style: TextStyle(fontSize: buttonTextSize),)
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 24, 0, 0),
+                    child: ElevatedButton(
+                        onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(double.infinity, 50),
+                          backgroundColor: darkNavy
+                        ),
+                        child: Text('작성하기',style: TextStyle(fontSize: contentTextSize),)
+                    ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 14, 0, 30),
+                    child: ElevatedButton(
+                        onPressed: (){
+                          Navigator.pop(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(double.infinity, 50),
+                          backgroundColor: Color(0xffA2A2A2)
+                        ),
+                        child: Text('취소하기',style: TextStyle(fontSize: contentTextSize),)
+                    ),
+                  ),
+                ],
+              ),
             )
           ),
           Expanded(child: SizedBox()),
@@ -242,3 +186,4 @@ class _NoticeAIState extends State<NoticeAI> {
     );
   }
 }
+
