3325cafc5fc8dbf08ba00273555ccc32aac4e25d
오하늘 <moho009804@gmail.com>
2023-05-14 22:20:18 +0900

    Feat: 디자인 수정

--- app/lib/components/common/text_form_field_custom.dart
+++ app/lib/components/common/text_form_field_custom.dart
@@ -26,32 +26,35 @@ class TextFormFieldCustom extends StatelessWidget {
       fillColor = enabled ? cardPink : Colors.white;
     }
 
-    return TextFormField(
-      keyboardType: TextInputType.text,
-      textInputAction: TextInputAction.next,
-      onChanged: onChanged,
-      obscureText: obscureText,
-      enabled: enabled,
-      decoration: InputDecoration(
-        contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
-        hintText: hint,
-        hintStyle: TextStyle(
-          color: enabled ? Colors.black87: Colors.black87,
-          fontSize: 14,
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-          borderSide: BorderSide(color: Colors.black26),
-        ),
-        disabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-          borderSide: BorderSide(color: Colors.black26),
-        ),
-        filled: true,
-        fillColor: fillColor,
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-          borderSide: BorderSide(color: darkYellow),
+    return Container(
+     height: 37,
+      child: TextFormField(
+        keyboardType: TextInputType.text,
+        textInputAction: TextInputAction.next,
+        onChanged: onChanged,
+        obscureText: obscureText,
+        enabled: enabled,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 2, horizontal: 15),
+          hintText: hint,
+          hintStyle: TextStyle(
+            color: enabled ? Colors.black87: Colors.black87,
+            fontSize: 13,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            borderSide: BorderSide(color: Colors.black26),
+          ),
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            borderSide: BorderSide(color: Colors.black26),
+          ),
+          filled: true,
+          fillColor: fillColor,
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            borderSide: BorderSide(color: darkYellow),
+          ),
         ),
       ),
     );
