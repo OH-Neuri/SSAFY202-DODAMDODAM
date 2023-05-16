d08f7f6d45995182d791ab79fa2b769c1ef30aad
김나현 <skgusdl7403@naver.com>
2023-05-16 20:41:12 +0900

    Design: 기타 이미지 교체

---
+++ app/lib/api/notify_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:app/api/http_header.dart';
+import 'package:app/api/url_mapping.dart';
+import 'package:app/models/notice/notice_regist_response_model.dart';
+import 'package:http/http.dart' as http;
+
+class NotifyService {
+  static void sendNotify(NotifyModel notify) async {
+    try {
+      String URL = '${baseUrl}notify';
+      final res = await http.post(
+        Uri.parse(URL),
+        headers: postHeaders,
+        body: jsonEncode(notify.toJson())
+      );
+      if (res.statusCode == 200) {
+        print('알림성공');
+      }else{
+        print('알림실패');
+      }
+    } catch(e) {
+      print(e);
+    }
+  }
+}
