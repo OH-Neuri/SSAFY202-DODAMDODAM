ae9f22909f5276a702bc0beb281fe2e54c303704
김나현 <skgusdl7403@naver.com>
2023-05-17 20:11:39 +0900

    Feat: 페이지 헤더 어린이집 이름 수정

--- frontend/dodamdodam/src/components/common/pageHeader.tsx
+++ frontend/dodamdodam/src/components/common/pageHeader.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function PageHeader(props: { name: string }) {
   const { name } = props;
+  const [ kindergarten, setKindergarten ] = useState<string>('')
 
+  useEffect(()=>{
+    const kinder = sessionStorage.getItem('kindergarten');
+    if(kinder){
+      setKindergarten(kinder)
+    }
+  },[])
   return (
     <>
       <div className="flex flex-col mt-10 mb-10 ml-10">
         <div className="flex flex-col justify-center ml-3">
-          <div className="text-[20px]">소정어린이집</div>
+          <div className="text-[20px]">{kindergarten}</div>
           <div className="text-[40px] font-preB text-[#4F4F4F]">{name}</div>
         </div>
         <hr className="mt-[20px]" />
