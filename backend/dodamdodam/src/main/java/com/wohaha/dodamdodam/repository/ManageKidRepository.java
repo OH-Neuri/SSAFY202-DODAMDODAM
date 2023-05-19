376fb824c1204286e3fdca0d6d5227403904883d
yunsuhee <suheeyun97@gmail.com>
2023-05-19 10:41:29 +0900

    Fix:브랜치 정리

--- backend/dodamdodam/src/main/java/com/wohaha/dodamdodam/repository/ManageKidRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wohaha.dodamdodam.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ManageKidRepository extends JpaRepository<Class, String>, ManageKidRepositoryCustom {
-
-}
